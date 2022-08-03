050db32f1ef376039169907c4cad6ca9583fb4fd
Mrinal kanti sardar <mrinalkantisardar007@gmail.com>
2022-08-03 17:46:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div className="navbar__main">
+          <nav className="nav__main">React Docker</nav>
+        </div>
+        <body className="main__body">
+          <img
+            className="images"
+            src="https://img.icons8.com/fluency/344/docker.png"
+            alt="Docker"
+          />
+        </body>
+      </>
     </div>
   );
 }
